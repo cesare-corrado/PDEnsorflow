8c7828d445aa085f70c5722cc886d794267ae2aa
Cesare Corrado <cesare.corrado@kcl.ac.uk>
2023-01-19 14:07:51 +0100

    extended the models to heterogeneous ionic parameters

--- PDEnsorflow/gpuSolve/ionic/mms2v.py
+++ PDEnsorflow/gpuSolve/ionic/mms2v.py
@@ -45,22 +45,59 @@ class ModifiedMS2v:
         Math Biosci. 216 Nov;281:46-54.
         This class implements the transmembrane potential within the interval [0,1]
     """
-        
 
+    def __init__(self, props=None):
+        rankmax = 0
+        self._tau_in    = tf.constant(0.1)
+        self._tau_out   = tf.constant(9.0)
+        self._tau_open  = tf.constant(100.0)
+        self._tau_close = tf.constant(120.0)
+        self._u_gate    = tf.constant(0.13)
+        self._u_crit    = tf.constant(0.13)
+        if props is not None:
+            for key in self.__dict__.keys():
+                prop_key = key[1:]
+                if prop_key in props.keys():
+                    prop_value = tf.constant(props[prop_key])
+                    prop_rank  = tf.rank(prop_value).numpy()
+                    rankmax    = max(rankmax,prop_rank)
+                     setattr(self, key, prop_value) 
+            if rankmax>0:
+                newrank = np.ones(shape=rankmax,dtype=int)
+                for key in self.__dict__.keys():
+                    prop_value = getattr(self,key)
+                    prop_rank  = tf.rank(prop_value).numpy()
+                    if prop_rank<rankmax:
+                        prop_value = tf.constant(prop_value.numpy(),shape= newrank,dtype=np.float32 )
+                        setattr(self, key, prop_value)
+                
+    def tau_in(self):
+        return(self._tau_in)        
+
+    def tau_out(self):
+        return(self._tau_out)        
+
+    def tau_open(self):
+        return(self._tau_open)        
+
+    def tau_close(self):
+        return(self._tau_close)        
+
+    def u_gate(self):
+        return(self._u_gate)        
+
+    def u_crit(self):
+        return(self.u_crit)        
+
+ 
     @tf.function
     def differentiate(self, U, H):
         """ the state differentiation for the 2v model """
         # constants for the modified Mitchell Schaeffer 2v left atrial action potential model
-        tau_in    = tf.constant(0.1)
-        tau_out   = tf.constant(9.0)
-        tau_open  = tf.constant(100.0)
-        tau_close = tf.constant(120.0)
-        u_gate    = tf.constant(0.13)
-        u_crit    = tf.constant(0.13)
-        J_in  =  -1.0 * H * U * (U-u_crit) * (1.0-U)/tau_in
-        J_out =  (1.0-H)*U/tau_out
+        J_in  =  -1.0 * H * U * (U-self._u_crit) * (1.0-U)/self._tau_in
+        J_out =  (1.0-H)*U/self._tau_out
         dU    = - (J_in +J_out)
-        dH = tf.where(U > u_gate, -H / tau_close, (1.0 - H) / tau_open)        
+        dH = tf.where(U > self._u_gate, -H / self._tau_close, (1.0 - H) / self._tau_open)        
         return dU, dH
 
 
