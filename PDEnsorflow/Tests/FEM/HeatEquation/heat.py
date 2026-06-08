776b09d10ed0fcbdc73546c94e3a1f12c652e23e
Cesare Corrado <ccorrado@imperial.ac.uk>
2026-06-08 13:31:58 +0100

    modified FEM examples

--- PDEnsorflow/Tests/FEM/HeatEquation/heat.py
+++ PDEnsorflow/Tests/FEM/HeatEquation/heat.py
@@ -104,7 +104,7 @@ if __name__=='__main__':
                      'nx':model.domain().Pts().shape[0]
                      })
     model.finalize_for_run()
-    im.imshow(model.U().numpy())
+    im.imshow(model.U())
 
     # ---- external time loop (HeatSolver advances one step per call) --------
     then = time.time()
@@ -113,7 +113,7 @@ if __name__=='__main__':
         ctime += model.dt()
         model.step(ctime)
         if im and i % model.dt_per_plot() == 0:
-            im.imshow(model.U().numpy())
+            im.imshow(model.U())
     elapsed = (time.time() - then)
     print('solution, elapsed: %f sec' % elapsed)
     if im:
