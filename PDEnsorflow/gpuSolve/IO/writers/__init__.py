e9818908d920de7cdab2379f986890d07442c3d3
Cesare Corrado <ccorrado@imperial.ac.uk>
2026-06-08 13:21:48 +0100

    implemented new base writer to avoid GPU to CPu transfers

--- PDEnsorflow/gpuSolve/IO/writers/__init__.py
+++ PDEnsorflow/gpuSolve/IO/writers/__init__.py
@@ -1,7 +1,8 @@
 """
-writers and plotters  
+writers and plotters
 submodule of IO
 Contains:
+    * BaseWriter: a base class that manages the GPU buffering/chunking of the solution
     * ResultWriter: a class to write results in npy format
     * VedoPlotter: a class implementing a plotter based on ved library
     * CarpMeshWriter: a class to write the mesh in carp {.pts, .elem, .lon} format
@@ -22,6 +23,7 @@ def version():
   return(verstr)
 
 
+from gpuSolve.IO.writers.basewriter import BaseWriter
 from gpuSolve.IO.writers.resultwriter import ResultWriter
 #from gpuSolve.IO.writers.vedoplotter import VedoPlotter
 from gpuSolve.IO.writers.carpmeshwriter import CarpMeshWriter
